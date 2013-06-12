70512011c22a47c5ed55d7a350680cf07b569e2f
digvijay88 <ssr.digvijay88@gmail.com>
2013-06-12 23:14:16 +0530

    committing the GT annotation tool

---
+++ apps/tool.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<cstdio>
+#include<cv.h>
+#include<highgui.h>
+#include<boost/filesystem.hpp>
+#include<vector>
+#include<string>
+#include<cstring>
+
+using namespace std;
+using namespace cv;
+using namespace boost::filesystem;
+
+typedef vector<path> vec;
+
+FILE *fp;
+
+string convertInt(int number)
+{
+	stringstream ss;//create a stringstream
+	ss << number;//add number to the stream
+	return ss.str();//return a string with the contents of the stream
+}
+
+void mousehandler(int event,int x, int y,int flags,void *param)
+{
+	if(event == CV_EVENT_LBUTTONDOWN)
+	{
+		cout << x << " " << y << " "<< endl;
+	       	string st = convertInt(x) + ' ' + convertInt(y) + '\n';
+		fputs(st.c_str(),fp);
+	}
+}
+
+int main(int argc,char *argv[])
+{
+	path p (argv[1]);   // p reads clearer than argv[1] in the following code
+	fp = fopen("coordinates.txt","w+");
+
+	if (exists(p))    // does p actually exist?
+	{
+		if (is_directory(p))      // is p a directory?
+		{
+			cout << p << " is a directory. Thank You!!\n";
+			vec v;
+			copy(directory_iterator(p), directory_iterator(), back_inserter(v));
+			vector<string> images_list;
+			cvNamedWindow("AnnotateGT",CV_WINDOW_NORMAL);
+			cvSetMouseCallback("AnnotateGT", mousehandler);
+			int cnt = 0;
+			for (vec::const_iterator it (v.begin()); it != v.end(); ++it)
+			{
+				//				cout << "   " << *it << '\n';
+				string temp = it->string();
+				if(temp.compare(temp.size()-4,4,".png") == 0 || temp.compare(temp.size()-4,4,".jpg") == 0)
+				{
+					cout << temp << endl;
+					Mat img = imread(temp);
+					temp = temp + '\n';
+					fputs(temp.c_str(),fp);
+					imshow("AnnotateGT",img);
+					cvWaitKey(0);
+					if(cnt == 2)
+						break;
+					cnt++;
+					//					img.deallocate();	
+				}
+			}
+			cvDestroyWindow("AnnotateGT");
+		}
+		else
+			cout << p << " is not a directory. Please pass a directory path as argument\n";
+	}
+	else
+		cout << p << " does not exist\n";
+	fclose(fp);
+	return 0;
+}
