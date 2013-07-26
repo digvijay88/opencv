878e9815339a402f2f9511283dcebe4721aa6571
digvijay88 <ssr.digvijay88@gmail.com>
2013-07-26 22:59:57 +0530

    removing bugs

--- samples/cpp/barcode_detect.cpp
+++ samples/cpp/barcode_detect.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/utility.hpp>
 #include "opencv2/barcode1D.hpp"
+#include "opencv2/highgui.hpp"
 
 
 #include<iostream>
@@ -18,34 +19,35 @@ static int parseCmdArgs(int argc, char** argv)
     cout << "Enter the image name" << endl;
     return -1;
   }
-  for(int i=1;i<argc,i++)
+  for(int i=1;i<argc;i++)
   {
     if(string(argv[i]) == "-img")
     {
       img_name = string(argv[i+1]);
-      cout << "Loading image: " << img_name << endl;
       i++;
     }
   }
+  return 1;
 }
 
-int main(int argc,const char** argv)
+int main(int argc,char** argv)
 {
   int retval = parseCmdArgs(argc, argv);
-  if(retval)
-    return -1;
-  
+//  if(retval)
+//    return -1;
+  cout << image_name << endl;  
   Mat image = imread(img_name.c_str(),IMREAD_GRAYSCALE);
   if(image.empty())
   {
     cout << "Unable to read the input image!!" << endl;
     return 0;
   }
-  
+  cout << "went in" << endl; 
   vector<RotatedRect> barcode_rect;
   vector<Point> barcode_cpoints;
   Mat output;
-  preprocessImage(image,output);
+  KatonaNyu detectr;
+  detectr.preprocessImage(image,output);
 
   return 0;
 }
